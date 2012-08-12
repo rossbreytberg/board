251f6582c75fed55e38ffcbcfe5d89a5013a9ee4
Ross <rossbreytberg@gmail.com>
2012-08-11 18:19:18 -0700

    changed css animations to transitions

--- static/js/index.js
+++ static/js/index.js
@@ -9,13 +9,11 @@ window.onload = function() {
         pageY = 0,
         THUMBNAIL_SIZE = 300,
         THUMBNAIL_BORDER_WIDTH = 5,
-        THUMBNAIL_SHADOW_SIZE = 2,
-        THUMBNAIL_SHADOW_BLUR = 2,
-        THUMBNAIL_SHADOW_COLOR = "rgba(0, 0, 0, 0.5)",
         POSTIMAGE_ANIMATION_DURATION = "500ms",
         POSTIMAGE_ANIMATION_TIMING_FUNCTION = "ease-in-out",
         EXPAND_ANIMATION_DURATION = "500ms",
         EXPAND_ANIMATION_TIMING_FUNCTION = "ease-in",
+        EXPAND_BORER_SIZE_MULTIPLIER = 0.5,
         EXPAND_SIZE_MULTIPLIER = 0.95,
         HELPTIP_FADE_ANIMATION_DURATION = "3s",
         HELPTIP_FADE_ANIMATION_TIMING_FUNCTION = "ease";
@@ -42,6 +40,22 @@ window.onload = function() {
         evt.preventDefault();
     };
 
+    var getScaledDimensions = function(naturalWidth, naturalHeight, targetWidth, targetHeight) {
+        var aspectRatio = naturalWidth/naturalHeight;
+        var desiredWidth = targetWidth;
+        var desiredHeight = targetWidth/aspectRatio;
+        if (desiredWidth > targetWidth || desiredHeight > targetHeight) {
+            desiredWidth = targetHeight * aspectRatio;
+            desiredHeight = targetHeight;
+        }
+        if (desiredWidth > naturalWidth && desiredHeight > naturalHeight) {
+            desiredWidth = naturalWidth;
+            desiredHeight = naturalHeight;
+        }
+        return {"width": desiredWidth, "height": desiredHeight};
+    }
+
+    /*
     var animation = function(element, properties, startValues, endValues, duration, timingFunction, callback) {
         if (timingFunction == null) {
             timingFunction = "";
@@ -79,21 +93,6 @@ window.onload = function() {
         element.addEventListener("webkitAnimationEnd", onAnimationEnd);
         element.classList.add(tempClassName);
     };
-
-    var getScaledDimensions = function(naturalWidth, naturalHeight, targetWidth, targetHeight) {
-        var aspectRatio = naturalWidth/naturalHeight;
-        var desiredWidth = targetWidth;
-        var desiredHeight = targetWidth/aspectRatio;
-        if (desiredWidth > targetWidth || desiredHeight > targetHeight) {
-            desiredWidth = targetHeight * aspectRatio;
-            desiredHeight = targetHeight;
-        }
-        if (desiredWidth > naturalWidth && desiredHeight > naturalHeight) {
-            desiredWidth = naturalWidth;
-            desiredHeight = naturalHeight;
-        }
-        return {"width": desiredWidth, "height": desiredHeight};
-    }
 
     var expandImage = function(image) {
         if (expandedImage != null && expandedImage != image) {
@@ -168,6 +167,103 @@ window.onload = function() {
             animation(helpTip, ["opacity"], [1], [0], HELPTIP_FADE_ANIMATION_DURATION, HELPTIP_FADE_ANIMATION_TIMING_FUNCTION, function() {
                 helpTip.style.visibility = "hidden";
             });
+        }
+    };
+    */
+
+    var expandImage = function(image) {
+        if (expandedImage != null && expandedImage != image) {
+            expandedImage.onclick();
+        }
+        expandedImage = image;
+        var documentElement = document.documentElement;
+        var pageWidth = documentElement.clientWidth;
+        var pageHeight = documentElement.clientHeight;
+        var imageStyle = image.style;
+        var naturalWidth = parseInt(image.naturalWidth);
+        var naturalHeight = parseInt(image.naturalHeight);
+        var initialWidth = parseInt(imageStyle.width);
+        var initialHeight = parseInt(imageStyle.height);
+        var initialBorderWidth = parseInt(imageStyle.borderWidth);
+        var initialLeft = parseInt(imageStyle.left);
+        var initialTop = parseInt(imageStyle.top);
+        var targetDimensions = getScaledDimensions(naturalWidth, naturalHeight, pageWidth * EXPAND_SIZE_MULTIPLIER, pageHeight * EXPAND_SIZE_MULTIPLIER);
+        var targetWidth = targetDimensions.width - (initialBorderWidth * 2);
+        var targetHeight = targetDimensions.height - (initialBorderWidth * 2);
+        var targetBorderWidth = initialBorderWidth;
+        var targetLeft = pageWidth/2 - imageBoard.offsetLeft - (targetWidth/2 + targetBorderWidth);
+        var targetTop = pageHeight/2 - imageBoard.offsetTop - (targetHeight/2 + targetBorderWidth);
+        image.onclick = null;
+        var shrinkEnd = function() {
+            image.removeEventListener("webkitTransitionEnd", shrinkEnd);
+            imageStyle.zIndex = 0;
+        };
+        var expandEnd = function() {
+            image.removeEventListener("webkitTransitionEnd", expandEnd);
+            imageStyle.zIndex = 1;
+        };
+        image.addEventListener("webkitTransitionEnd", expandEnd);
+        imageStyle.zIndex = 2;
+        imageStyle.width = targetWidth;
+        imageStyle.height = targetHeight;
+        imageStyle.left = targetLeft;
+        imageStyle.top = targetTop;
+        image.onclick = function() {
+            image.removeEventListener("webkitTransitionEnd", expandEnd);
+            expandedImage = null;
+            image.onclick = null;
+            image.addEventListener("webkitTransitionEnd", shrinkEnd);
+            imageStyle.width = initialWidth;
+            imageStyle.height = initialHeight;
+            imageStyle.left = initialLeft;
+            imageStyle.top = initialTop;
+            image.onclick = function() {
+                image.removeEventListener("webkitTransitionEnd", shrinkEnd);
+                expandImage(image);
+            };
+        };
+    };
+
+    var postImage = function(x, y, src) {
+        if (expandedImage != null) {
+            expandedImage.onclick();
+        }
+        var image = new Image();
+        image.src = src;
+        image.className = "img";
+        var initialWidth = 0;
+        var initialHeight = 0;
+        var initialBorderWidth = 0;
+        var initialLeft = x;
+        var initialTop = y;
+        var imageStyle = image.style;
+        imageStyle.width = initialWidth + "px";
+        imageStyle.height = initialHeight + "px";
+        imageStyle.left = initialLeft + "px";
+        imageStyle.top = initialTop + "px";
+        imageBoard.appendChild(image);
+        image.onload = function() {
+            var naturalWidth = image.naturalWidth;
+            var naturalHeight = image.naturalHeight;
+            var targetDimensions = getScaledDimensions(naturalWidth, naturalHeight, THUMBNAIL_SIZE, THUMBNAIL_SIZE);
+            var targetWidth = targetDimensions.width;
+            var targetHeight = targetDimensions.height;
+            var targetBorderWidth = THUMBNAIL_BORDER_WIDTH;
+            var targetLeft = x - (targetWidth/2 + targetBorderWidth);
+            var targetTop = y - (targetHeight/2 + targetBorderWidth);
+            imageStyle.borderWidth = initialBorderWidth + "px";
+            imageStyle.width = targetWidth + "px";
+            imageStyle.height = targetHeight + "px";
+            imageStyle.left = targetLeft + "px";
+            imageStyle.top = targetTop + "px";
+            imageStyle.borderWidth = targetBorderWidth;
+            image.onclick = function() {
+                expandImage(image);
+            };
+        };
+        if (showingHelpTip) {
+            showingHelpTip = false;
+            helpTip.style.opacity = 0;
         }
     };
 
